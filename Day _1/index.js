b0f6bc61aa6d43e43152345c8127464089000d6d
santosh302 <santoshkumarsy05@gmail.com>
2024-07-18 13:07:42 +0530

    first task complete

---
+++ Day _1/index.js
@@ -0,0 +1,44 @@
+/*  Variable and Data Types  */
+
+/*  Tasks/Activities  */
+
+/*  Activity 1: Variable Declaration  */
+
+
+/*  Task 1: Declare a variable using var, assign it a number, and log the value to the console.*/
+var i=10;
+console.log(i);
+
+/* Task 2: Declare a variable using let, assign it a string, and log the value to the console.*/
+
+
+/*  Activity 2: constant Declaration */
+
+/*  Tasks 3: Declare a variablel using const, assign it a boolean value, and log the value to the console. */
+
+
+ /* Activity 3: Data Types */ 
+ /* Task 4: Create variable of different data types(number, string, boolean, object, array) and log each variable's type using the typeof operator. */ 
+
+
+ /* Activity 4: Reassigning Variables */ 
+ /* Task 5: Declare a variable using let, assign it an inital value, reassign a new value, and log both value to the console. */
+
+
+/* Activity 5: Understanding const */  
+/* Task 6: Try reassigning a variable declared with const and observe the error. */
+
+
+/* Feature Request:  */
+/* Variable Types Console log: Write a script that variable of different data types and log both the value and type of each variable to the console.*/
+/* Reassignment Demo: Create a script that demonstrates the differents in behavior between let and const when it comes to reassignment. */
+
+
+/* Achievement */
+/* By the end of these activities, you will: */
+/*
+* Know how to declare variable using var, let, const.
+* Understand the different data types in JavaScript.
+*Be able to use the typeof operator to identify the data type of a variable.
+*Understand the concept of variable reassignment and the immutability of const variables.
+*/
