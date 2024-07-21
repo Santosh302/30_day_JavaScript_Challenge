c382e56cb0e8e24177eda40f30401be9518bd38b
santosh302 <santoshkumarsy05@gmail.com>
2024-07-21 12:28:43 +0530

    day 4 completed

--- Day_4/Activity_5.js
+++ Day_4/Activity_5.js
@@ -2,3 +2,20 @@
 Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
 Task 9:Write a program to print numbers from 1 to 10,but stop the loop when the number is 7 using the break statement
 */
+
+/* Task 8 */
+for(i=1; i<=10; i++){
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+
+
+/* Task 9 */
+for(j=1;j<=10; j++){
+    if(j==7){
+        break;
+    }
+    console.log(j);
+}
