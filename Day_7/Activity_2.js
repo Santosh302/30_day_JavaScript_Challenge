d6b94ee0cddb0d99d38cc0ff9d04307c812ef558
santosh302 <santoshkumarsy05@gmail.com>
2024-07-23 18:25:44 +0530

    update

--- Day_7/Activity_2.js
+++ Day_7/Activity_2.js
@@ -2,3 +2,28 @@
 Task 3: Add a method to the book object that returns a string with the book's title and author,  and log the result of calling this method.
 Task 4:Add a method to the book object that takes a parameter(Year) and updates the book's year property, then log the updated object.
 */
+
+/* const Book={
+    title:'Computer Basic',
+    author:'Santosh Kumar Yadav',
+    Year:'2024',
+    getDetails:function() {
+        return `${this.title} and ${this.author}`;
+    }
+}
+
+
+console.log(Book.getDetails()); */
+
+/* Task 4 */
+const Book={
+    title:'Computer Basic',
+    author:'Santosh Kumar Yadav',
+    Year:'2024',
+   updateYear:function(newYear){
+    this.Year= newYear
+   }
+}
+Book.updateYear(2025);
+
+console.log(Book.Year);
--- Day_7/Activity_3.js
+++ Day_7/Activity_3.js
@@ -2,3 +2,14 @@
 Task 5: Create a nested object representing a library with properties like name and books(an array of book objects), and log the library object to the console.
 Task 6:Access and log the name of the library and the titles of all the books in the library.
 */
+const Book={
+    title:'Computer Basic',
+    author:'Santosh Kumar Yadav',
+    Year:'2024',
+    getDetails:function() {
+        return `${this.title} and ${this.author}`;
+    }
+}
+
+
+console.log(Book.getDetails());
