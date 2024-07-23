be55d6caee00aa8f81c2b31487daf85b93964085
santosh302 <santoshkumarsy05@gmail.com>
2024-07-23 19:06:48 +0530

    update

--- Day_7/Activity_5.js
+++ Day_7/Activity_5.js
@@ -2,3 +2,18 @@
 Task 8:Use a for...in loop to iterate over the properties of the book object and log each property and its value.
 Task 9:Use Object.keys and Object.values methods to log all the keys and values of the book object.
 */
+
+/* Task 8 */
+const Book={
+    title:'Computer Basic',
+    author:'Santosh Kumar Yadav',
+    Year:'2024',
+}
+
+for(const key in Book){
+
+    console.log(`${key}:`, Book[key]);
+}
+/* Task 9 */
+console.log(Object.keys(Book));
+console.log(Object.values(Book));
--- Day_7/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const Book={};
-Book.title="Computer Basic";
-Book.author="Santosh Kumar Yadav";
-Book.Year=2024;
-console.log(Book);
