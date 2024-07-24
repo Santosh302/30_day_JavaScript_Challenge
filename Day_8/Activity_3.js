789d876884785d9bd61c82089e2bd884ce2134ae
santosh302 <santoshkumarsy05@gmail.com>
2024-07-24 13:39:56 +0530

    update

--- Day_8/Activity_3.js
+++ Day_8/Activity_3.js
@@ -2,3 +2,14 @@
 Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additional elements, and log the new array to the console.
 Task 6: Use the rest operator in function to accept an arbitrary number of arguments, sum them, and return the result.
 */
+
+const arr = [1,2,3,4,5];
+const arr1=[...arr, 6,7,8];
+console.log(arr1);
+
+
+function sum(...numbers){
+    return numbers.reduce((acc, curr)=>acc+curr ,0);
+}
+console.log(sum(1,2,3,4));
+console.log(sum(1,2,8,4))
