e98af7fb69462976cc034de314130f5f90f21ff6
santosh302 <santoshkumarsy05@gmail.com>
2024-07-24 13:58:10 +0530

    update

--- Day_8/Activity_4.js
+++ Day_8/Activity_4.js
@@ -2,3 +2,16 @@
 Task 7:Write a function that takes two parameters and returns their product,
  with the second parameter having a default value of 1. Log the result of calling this function with and without the second parameter.
 */
+
+function product(product_1, product_2){
+    return [product_1, product_2];
+}
+const result=product('books', 'pen');
+console.log(result);
+
+
+function sum( a,  b=1){
+    return a+b;
+}
+console.log(sum(1, 5));
+console.log(sum(2));
