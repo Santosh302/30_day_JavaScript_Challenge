82d76f148d7453014ad467a5b285836c0a72fc3a
santosh302 <santoshkumarsy05@gmail.com>
2024-07-25 23:46:37 +0530

    update

--- Day_8/Activity_5.js
+++ Day_8/Activity_5.js
@@ -3,3 +3,30 @@ Task 8: Use enhanced object literals to create an object with methods and proper
 Task 9: Create an object with computed property names based on variables and log the object to the console.
 */
 
+const person={
+
+    firstName:'Santosh Kumar ',
+    lastName:'Yadav',
+    age:30,
+
+    //method to get full name
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`;
+    },
+    //Method to introduce the person
+    introduce(){
+        console.log(`Hi, I'm ${this.getFullName()} and I'm ${this.age} years old.`);
+    },
+
+    //Porperty with dynamic key
+    ['greetingMessage']:'Hello, world !'
+
+};
+
+console.log(person); //Log the object to the console
+
+//Log the result of methods
+
+console.log(person.getFullName()); //Outputs:John Doe
+person.introduce(); 
+console.log(person.greetingMessage);
