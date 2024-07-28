beab0c12e645ba672652278981de5fa5a894bd9d
santosh302 <santoshkumarsy05@gmail.com>
2024-07-29 00:20:11 +0530

    Activity 2 completed

--- Day_9/Activity_2.js
+++
@@ -1,4 +0,0 @@
-/* Activity 2: Creating and Appending Elements 
-Task 3: Create a new div element with some text content and append it to the body.
-Task 4: Create a new li element and add it to an existing ul list.
-*/
