78f751d3f9d27956302b30aa1a19163a8d3bf9a1
santosh302 <santoshkumarsy05@gmail.com>
2024-07-29 01:27:58 +0530

    day 9 completed

--- Day_9/Activity_5.js
+++
@@ -1,4 +0,0 @@
-/* Activity 5: Event Handling
-Task 9: Add a click event listener to a button that changes the text content of a paragraph.
-Task 10: Add a mouseover event listner to an  element that changes its border color.
-*/
