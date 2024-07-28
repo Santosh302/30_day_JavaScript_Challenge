d3a3a4a3ced52e0d88ed704a7c1ebf6a1ec71ae6
santosh302 <santoshkumarsy05@gmail.com>
2024-07-29 01:09:05 +0530

    day 9 activity 4 completed

--- Day_9/Activity_4.js
+++
@@ -1,4 +0,0 @@
-/* Activity 4: Modifying Attributes and Classes
-Task 7: Select an HTML element and change one of its attributes (e.g, src of an img tag).
-Task 8: Add and remove a CSS class to/from an HTML element.
-*/
