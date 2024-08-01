43909b87e11690ce91f9a0a1acdbb32ab5c75c28
santosh302 <santoshkumarsy05@gmail.com>
2024-08-01 22:06:35 +0530

    update

---
+++ Day_20/index.js
@@ -0,0 +1,42 @@
+/* LocalStorage and SessionStorage */
+
+/* Activity 1: Understanding Local Storage
+Task 1: Write a script to save a string value to localStorage and retrive it. Log the retrived value.
+Task 2:Write a script to save an object to localStorage by converting it to a json string.
+Retrive and parse the object, then log it,
+*/
+
+/* Activity 2:Using LocalStorage
+Task 3:Create a simple form that user input(name and email) to localStorage when submitted. Retrive and display the saved data on page load.
+Task 4:Write a script to remove an item from localStorage. Log the localStorage content before and after removal. 
+*/
+
+/* Activity 3: Uderstanding SessionStorage
+Task 5: Write a script to save a string value to sessionStorage and retrieve it. Log the retrieved value.
+Task 6: Write a script to save an object to sessionStorage by converting it to a JSON string. 
+Retrieve and parse the object, then log it.
+*/
+
+/* Activity 4: Using SessionStorage
+Task 7: Create a simple form that saves user input(e.g, name and email) to sessionStorage 
+when submitted. Retrieve and display the saved data on page load.
+Task 8:Write a script to remove an item from sessionStoragea. Log the sessionStorage content before and after removal.
+*/
+
+/* Activity 5: Comparing LocalStorage and SessionStorage
+Task 9: Write a function that accepts a key and value, and saves the value to both localStorage and sessionStorage. Retrieve and log the vlaues from both storage mechanisms.
+Task 10:Write a function that clear all data from both localStorage and sessionStorage.Verify that both storage are empty.
+*/
+
+/* Feature Request:
+1.LocalStorage Script:Write a script that saves, retrieves, and removes items from localStorage, and displays the saved data on page load.
+2.SessionStorage Script: Create a script that saves, retrieves, and removes items from session Storage, and display the saved data on page load.
+3.Storage Comparion Script: Write a script that saved data to both localStorage and sessionStorage, retrieves the data, and compares the results.
+4.Clear Storage Script:Create a script that clears all data from both localStorage and sessionStorage, and verifies the operation. */
+
+/* Achievement: 
+*Understand how to use localStorage and sessionStorage for persistent and session-specific data storage.
+*Save, retrieve and remove data from both localStorage and sessionStorage.
+*Implement from data storage using localStorge and sessionStorage.
+*Compare and contrast the user cases for local Storage and sessionStorage.
+*/
