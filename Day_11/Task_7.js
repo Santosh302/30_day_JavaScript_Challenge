6ae68fb169f861f98ffb370b183fa355997531ca
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:24:03 +0530

    update

--- Day_11/Task_7.js
+++ Day_11/Task_7.js
@@ -1,2 +1,16 @@
 /* Task 7: Use the fetch API to get data from a public API and log the response data to the console using async/await.
  */
+
+async function fetchData(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        if(!response.ok){
+            throw new Error('Network response was not ok' +response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+fetchData();
