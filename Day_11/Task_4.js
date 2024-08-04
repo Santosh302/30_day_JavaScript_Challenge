788ca1db5481940de45297e5c9f04ef7c6bba1b5
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:05:38 +0530

    update

--- Day_11/Task_4.js
+++ Day_11/Task_4.js
@@ -1,3 +1,21 @@
 /* Activity 3: Using Async/Await :-
 Task 4:Write an async function that waits for a promise to resolve and then logs the resolved value.
 */
+
+function resolveAfter2Seconds(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Promise resolve after 2 seconds");
+        }, 2000);
+    });
+}
+
+async function logResolvedValue(){
+    try{
+        const result= await resolveAfter2Seconds();
+        console.log(result);
+    }catch(error){
+        console.error("Error", error);
+    }
+}
+logResolvedValue();
