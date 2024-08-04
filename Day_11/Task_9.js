9bdff69948976b4df0f68b807c16bf628ebbe829
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:42:14 +0530

    Day 11 all tasks completed.

--- Day_11/Task_9.js
+++ Day_11/Task_9.js
@@ -1,2 +1,34 @@
 /* Task 9: Use Promise.race to log the value of the first promise that resolves among multiple promises.
  */
+
+function promise1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 1 resolved");
+        }, 3000);
+    });
+}
+
+function promise2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 2 resolved");
+        }, 2000);
+    });
+}
+
+function promise3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 3 resolved");
+        }, 1000);
+    });
+}
+
+Promise.race([promise1(), promise2(), promise3()])
+    .then((value) => {
+        console.log(value); // "Promise 3 resolved"
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
