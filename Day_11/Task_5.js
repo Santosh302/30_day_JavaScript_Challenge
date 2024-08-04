67692d1534641f4e86a8f0c19e504ff457c4f5ba
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:11:20 +0530

    update

--- Day_11/Task_5.js
+++ Day_11/Task_5.js
@@ -1,2 +1,20 @@
 /* Task 5:Write an async function that handles a rejected promise using try-catch and logs the error message.
  */
+function rejectAfter2Seconds(){
+    return new Promise((_ , reject)=>{
+        setTimeout(()=>{
+            reject(new Error("Promise rejected after 2 seconds"));
+        }, 2000);
+    });
+}
+
+async function handleRejectedPromise(){
+    try{
+        const result= rejectAfter2Seconds();
+        console.log(result);
+    } catch (error){
+        console.log("Error:", Error.message);
+    }
+}
+
+handleRejectedPromise();
