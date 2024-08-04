10e0ed06ccbaa3c1ff7f59c709f2b7d9b5ea1f79
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:17:32 +0530

    update

--- Day_11/Task_6.js
+++ Day_11/Task_6.js
@@ -1,3 +1,18 @@
+const { response } = require("express");
+
 /* Activity 4: Feching Data from an API 
 Task 6: Use the fetch API to get data from a public API and log the response data to the console using promises.
 */
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then(response=>{
+    if(!response.ok){
+        throw new Error('Network response was not ok', +response.statusText);
+    }
+    return response.json();
+})
+.then(data=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log('There has been a problem with your fetch operation:',error);
+});
