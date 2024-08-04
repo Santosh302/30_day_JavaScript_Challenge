9f3f359970379b7aca5d142a49099b44453e2e10
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:37:37 +0530

    update

--- Day_11/Task_8.js
+++ Day_11/Task_8.js
@@ -1,3 +1,35 @@
 /* Activity 5: Concurrent Promises 
 Task 8:Use Promise.all to await for multiple promises to resolve and then log all their values.
 */
+
+function promise1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 1 resolved");
+        }, 1000);
+    });
+}
+
+function promise2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 2 resolved");
+        }, 2000);
+    });
+}
+
+function promise3() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Promise 3 resolved");
+        }, 3000);
+    });
+}
+
+Promise.all([promise1(), promise2(), promise3()])
+    .then((values) => {
+        console.log(values); // ["Promise 1 resolved", "Promise 2 resolved", "Promise 3 resolved"]
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
