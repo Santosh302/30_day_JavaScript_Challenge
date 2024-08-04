23c4f671dfeccc402ebbec0ae71f4102a5d69f32
santosh302 <santoshkumarsy05@gmail.com>
2024-08-04 15:00:21 +0530

    update

--- Day_11/Task_3.js
+++ Day_11/Task_3.js
@@ -2,3 +2,27 @@
 Task 3: Create a sequence  of promises that simulate fetching data from a server. 
 Chain the promises to log messages in specific order.
 */
+
+function fetchDataFromServer(step, delay){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(`Data from step ${step} `);
+        }, delay);
+    });
+}
+
+fetchDataFromServer(1, 1000)
+.then((message1)=>{
+    console.log(message1);
+    return fetchDataFromServer(2, 1000);
+})
+.then((message2)=>{
+    console.log(message2);
+    return fetchDataFromServer(3, 1000);
+})
+.then((message3)=>{
+    console.log(message3);
+})
+.catch((error)=>{
+    console.error("Error fetching data:", error);
+});
