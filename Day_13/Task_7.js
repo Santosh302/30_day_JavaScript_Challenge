4d76256ea7fe4c2d139f2c48bc10bbffb214d59f
santosh302 <santoshkumarsy05@gmail.com>
2024-08-05 23:15:15 +0530

    update

--- Day_13/Task_7.js
+++ Day_13/Task_7.js
@@ -1,3 +1,15 @@
 /* Task 7: Install a third-party module(e.g., axios) using npm.
  Import and use this module to make a network request in a script.
  */
+
+ // Import the axios module
+const axios = require('axios');
+
+// Make a network request
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    console.log('Data:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
