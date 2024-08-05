b39691d9a88c4d4973e16a1a894cb8ad8e66c5fa
santosh302 <santoshkumarsy05@gmail.com>
2024-08-05 23:02:39 +0530

    update

--- Day_13/Task_4.js
+++ Day_13/Task_4.js
@@ -1,2 +1,8 @@
 /* Task 4: Create a module that exports a single function using default export. Import and use this function in another script.
  */
+
+function greet(name){
+    return `Hello, ${name}`;
+}
+
+module.exports= greet;
---
+++ Day_13/taskFourmodule.js
@@ -0,0 +1,6 @@
+
+const greet = require('./Task_4');
+
+const greetingMessage= greet('Santosh Kumar Yadav');
+
+console.log(greetingMessage);
