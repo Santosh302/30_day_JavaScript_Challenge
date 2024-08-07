093785e55445b8302f13f2e40c126ce1e1fb2764
santosh302 <santoshkumarsy05@gmail.com>
2024-08-07 09:33:05 +0530

    update

--- Day_15/index.js
+++ Day_15/index.js
@@ -1,8 +1,10 @@
 /* Closures */
 
 /* Activity 1: Understanding Closures 
-Task 1: Write a function that returns another function, where the inner function accesses a variable from the outer function's scope. Call the inner function and log the result.
-Task 2: Create a closure that maintains a private counter. Implement functions to increment and get the current value of the counter.
+Task 1: Write a function that returns another function, where the inner function accesses a 
+variable from the outer function's scope. Call the inner function and log the result.
+Task 2: Create a closure that maintains a private counter. 
+Implement functions to increment and get the current value of the counter.
 */
 
 /* Activity 2: Practical Closures
