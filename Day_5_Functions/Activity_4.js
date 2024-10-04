a94752a0f25e5537a948e8cb086e6c56035d2584
santosh302 <santoshkumarsy05@gmail.com>
2024-10-04 15:00:30 +0530

    update

--- Day_5_Functions/Activity_4.js
+++ Day_5_Functions/Activity_4.js
@@ -2,3 +2,17 @@
  Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
  Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
  */
+
+
+ function para(a, b=2){
+    return a*b;
+ }
+ console.log(para(4));
+ console.log(para(4, 5));
+
+
+ function greet(name, age=24){
+    return `Hello ${name} You are ${age} Years old`;
+ }
+
+ console.log(greet("Santosh Kumar Yadav"))
