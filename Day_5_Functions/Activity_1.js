0dad98d214265f53dcfa4def2b9aeffe2aa1dff6
santosh302 <santoshkumarsy05@gmail.com>
2024-10-04 14:46:41 +0530

    update

--- Day_5_Functions/Activity_1.js
+++ Day_5_Functions/Activity_1.js
@@ -2,3 +2,24 @@
 Task 1: Write a function to check if a number is even or odd and log the result to the conosole.
 Task 2: Write a function to calculate the square of a number and return the result.
 */
+
+/* function OddandEven(){
+    let number= 20
+    if(number%2 === 0){
+        console.log("Number is even");
+    }
+    else
+    {
+        console.log("Number is odd");
+    }
+
+}
+OddandEven(); */
+
+
+function Square(no){
+
+    return no*no ;
+}
+const result = Square(2);
+console.log("Square of these number are", result);
--- Day_5_Functions/Activity_2.js
+++ Day_5_Functions/Activity_2.js
@@ -2,3 +2,18 @@
  Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
  Task 4: Write a function expression to concatenate two string and return the result.
  */
+
+
+ const max= function(a, b){
+    return a>b? a: b;
+ }
+ const result =max(5 ,3);
+ console.log(result);
+
+
+ const joinstring = function(str1, str2){
+    return str1+str2;
+ }
+ const result1= joinstring("Hello", "Santosh");
+
+ console.log(result1)
